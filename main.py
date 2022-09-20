40f1788fc4a8fae599ec4103723b85108bd063f6
Shane Tanner <shanetanner03@gmail.com>
2022-09-20 00:43:28 -0500

    I revamped the main function to run the program. I also updateda couple other functions.

--- main.py
+++ main.py
@@ -12,51 +12,60 @@ def random_selector(list_to_select_from):
     selection = random.choice(list_to_select_from)
     return selection
 
-def print_results():
-    print('Destination: ' + print_list[0])
-    print('Restaurant: ' + print_list[1])
-    print('Transportation: ' + print_list[2])
-    print('Entertainment: ' + print_list[3])
+def print_results(list):
+    print('Destination: ' + list[0])
+    print('Restaurant: ' + list[1])
+    print('Transportation: ' + list[2])
+    print('Entertainment: ' + list[3])
 
-def unsatisfied_selection(list, list_position, list_index):
-        list.remove(list_position)
-        print_list[list_index] = random_selector(list)
-        print_results()
-        check_satisfaction()
+def unsatisfied_selection(lists, list_position, list_index, list):
+        lists.remove(list_position)
+        if lists.__len__() < 1:
+            list[list_index] = input('Tell me what you want. Write in your choice. ')
+            print_results(list)
+            check_satisfaction(list)
+        else:
+            list[list_index] = random_selector(lists)
+            print_results(list)
+            check_satisfaction(list)
 
-def check_satisfaction():
+def check_satisfaction(list):
     answer = input("Are you satisfied with your trip results? Yes or No ")
     while answer == 'no' or answer == 'No' or answer == 'N' or answer == 'n':
         change = input("What would you like to change; A: Destination, B: Restaurant, C: Transportation, or D: Entertainment? ")
         if change == 'Destination' or change == 'destination' or change == 'A' or change == 'a':
-            unsatisfied_selection(destinations, print_list[0], 0)
-            return print_list
+            unsatisfied_selection(destinations, list[0], 0, list)
+            return list
         
         elif change == 'Restaurant' or change == 'restaurant' or change == 'B' or change == 'b':
-            unsatisfied_selection(restaurants, print_list[1], 1)
-            return print_list
+            unsatisfied_selection(restaurants, list[1], 1, list)
+            return list
 
         elif change == 'Transportation' or change == 'transportation' or change == 'C' or change == 'c':
-            unsatisfied_selection(modes_of_transportation, print_list[2], 2)
-            return print_list
+            unsatisfied_selection(modes_of_transportation, list[2], 2, list)
+            return list
 
         elif change == 'Entertainment' or change == 'entertainment' or change == 'D' or change == 'd':
-            unsatisfied_selection(forms_of_entertainment, print_list[3], 3)
-            return print_list
+            unsatisfied_selection(forms_of_entertainment, list[3], 3, list)
+            return list
     while answer == 'yes' or answer == 'Yes' or answer == 'Y' or answer == 'y':
-        print(f"You will be traveling to {print_list[0]} by {print_list[2]}, where you will be eating at a {print_list[1]} and going {print_list[3]} on your Day Trip! I hope you have a great time!")
-        return print_list
-    else:
-        answer = input('That is not a valad answer. Please provide a Yes or No answer. ')
+        day_trip_confirmed(list)
+        break
 
+def day_trip_confirmed(list):
+    print(f"You will be traveling to {list[0]} by {list[2]}, where you will be eating at a {list[1]} and going {list[3]} on your Day Trip! I hope you have a great time!")
+        
 def day_trip_generator():
-
     destination = random_selector(destinations)
     restaurant = random_selector(restaurants)
     transportation = random_selector(modes_of_transportation)
     entertainment = random_selector(forms_of_entertainment)
     return [destination, restaurant, transportation, entertainment]
 
-print_list = day_trip_generator()
-print_results()
-check_satisfaction()
+def main():
+    print_list = day_trip_generator()
+    print_results(print_list)
+    check_satisfaction(print_list)
+    
+
+main()
